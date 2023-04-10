89144f5e070f517fe37c6b93a88937b3fb0c7af5
Robert Važan <robert.vazan@tutanota.com>
2023-04-10 17:50:10 +0000

    Scaffolding for the adapter

--- scripts/configure.py
+++ scripts/configure.py
@@ -5,10 +5,9 @@ import rvscaffold as scaffold
 
 class Project(scaffold.Java):
     def script_path_text(self): return __file__
-    def repository_name(self): return 'hookless'
-    def is_member_project(self): return True
-    def pretty_name(self): return 'Hookless'
-    def pom_description(self): return 'Reactive programming library.'
+    def repository_name(self): return 'hookless-time'
+    def pretty_name(self): return 'Reactive time for Hookless'
+    def pom_description(self): return 'Hookless reactive adapter for time classes from java.time package.'
     def inception_year(self): return 2015
     def jdk_version(self): return 17
     def stagean_annotations(self): return True
@@ -27,15 +26,5 @@ class Project(scaffold.Java):
         yield self.use('org.awaitility:awaitility:4.0.3', 'test')
         yield self.use('org.junit-pioneer:junit-pioneer:0.9.0')
         yield self.use_slf4j_test()
-    
-    def javadoc_links(self):
-        yield from super().javadoc_links()
-        yield 'https://noexception.machinezoo.com/javadocs/core/'
-        # No link to OpenTracing, because automatic modules cannot be linked.
-    
-    def documentation_links(self):
-        yield from super().documentation_links()
-        yield 'Concepts', 'https://hookless.machinezoo.com/concepts'
-        yield 'Adapters', 'https://hookless.machinezoo.com/adapters'
 
 Project().generate()
